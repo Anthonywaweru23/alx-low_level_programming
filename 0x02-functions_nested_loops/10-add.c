ca48e269a552ee1cb0c3b1de3e73c131d5722c48
Anthonywaweru23 <muturianthony2000@gmail.com>
2023-04-05 16:16:17 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two intergers
+ * @n: - the first number
+ * @h: - the second number
+ * Return: the addition of both
+ */
+int add(int n, int h)
+{
+	return (n + h);
+}
